e8fcbb0d8327e2d9096c526711bef2b9b94e08b3
JonathanScrok <jonathanwscrok@gmail.com>
2021-06-22 15:14:33 -0300

    Iniciando

---
+++ Seção 03/Projeto01/Site1CursoASPNET/Pages/About.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site1CursoASPNET.Pages {
+    public class AboutModel : PageModel {
+        public string Message { get; set; }
+
+        public void OnGet() {
+            Message = "Your application description page.";
+        }
+    }
+}
---
+++ Seção 03/Projeto01/Site1CursoASPNET/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site1CursoASPNET.Pages {
+    public class IndexModel : PageModel {
+        public void OnGet() {
+
+        }
+    }
+}
