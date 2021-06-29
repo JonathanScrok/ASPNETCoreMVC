0870699ea126885fd1e6c8aaaf11cff8f14c1adc
JonathanScrok <jonathanwscrok@gmail.com>
2021-06-29 20:11:08 -0300

    Subindo projeto correto.

---
+++ Seção 03/Projeto01/Site1CursoASPNET/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site1CursoASPNET.Controllers {
+    public class HomeController : Controller {
+
+        public ActionResult Index() {
+
+            return new ContentResult() { Content = "Olá Mundo!", ContentType = "text/plain" };
+        }
+
+    }
+}
---
+++ Seção 04/Projeto1/Site1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site1.Controllers {
+    public class HomeController : Controller{
+
+        public ActionResult Index() {
+            return new ContentResult() { Content = "Olá Mundo", ContentType = "text/plain" };
+        }
+    }
+}
