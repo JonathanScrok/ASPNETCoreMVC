4523f3ce5f4753ed3d3fcecc025abefdf82d8961
JonathanScrok <jonathanwscrok@gmail.com>
2021-08-06 15:31:43 -0300

    Criando envio de email do formulário

--- Seção 04/Projeto1/Site1/Controllers/ContatoController.cs
+++ Seção 04/Projeto1/Site1/Controllers/ContatoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Site1.Library.Mail;
 using Site1.Models;
 using System;
 using System.Collections.Generic;
@@ -14,13 +15,21 @@ namespace Site1.Controllers {
         public IActionResult ReceberContato([FromForm] Contato contato) {
 
             if (ModelState.IsValid) {
-                string conteudo = string.Format("Nome: {0}, Email: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
-                return new ContentResult() { Content = conteudo };
-            } else {
+                //string conteudo = string.Format("Nome: {0}, Email: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+                //return new ContentResult() { Content = conteudo };
+
+                EnviarEmail.EnviarMensagemContato(contato);
+
+                ViewBag.Mesagem = "Mensagem enviada com sucesso!";
                 return View("Index");
+
+            } else {
+
+                return View("Index");
+
             }
 
-           
+
         }
 
         /* Obter Dados Manualmente
