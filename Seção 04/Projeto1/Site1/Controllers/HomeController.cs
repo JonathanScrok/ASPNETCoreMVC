f1747180c0ada53b694caeb80089a9cff326f15a
JonathanScrok <jonathanwscrok@gmail.com>
2021-08-05 22:16:25 -0300

    Criação home page

--- Seção 04/Projeto1/Site1/Controllers/HomeController.cs
+++ Seção 04/Projeto1/Site1/Controllers/HomeController.cs
@@ -8,7 +8,8 @@ namespace Site1.Controllers {
     public class HomeController : Controller{
 
         public ActionResult Index() {
-            return new ContentResult() { Content = "Olá Mundo", ContentType = "text/plain" };
+            //return new ContentResult() { Content = "Olá Mundo", ContentType = "text/plain" };
+            return View();
         }
     }
 }
