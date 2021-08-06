6ba84be52b4c173017678ad8b83b53b51bb125cb
JonathanScrok <jonathanwscrok@gmail.com>
2021-08-05 23:04:25 -0300

    Formulário e armazenamento dos dados

--- Seção 04/Projeto1/Site1/Controllers/ContatoController.cs
+++ Seção 04/Projeto1/Site1/Controllers/ContatoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Site1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,28 @@ namespace Site1.Controllers {
         public IActionResult Index() {
             return View();
         }
+
+        public IActionResult ReceberContato([FromForm] Contato contato) {
+
+            string conteudo = string.Format("Nome: {0}, Email: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+            return new ContentResult() { Content = conteudo };
+        }
+
+        /* Obter Dados Manualmente
+
+        public IActionResult ReceberContato() {
+
+            //POST - Request.Form
+            //GET - Request.Query ou Request.QueryString
+            Contato contato = new Contato();
+            contato.Nome = Request.Form["nome"];
+            contato.Email = Request.Form["email"];
+            contato.Assunto = Request.Form["assunto"];
+            contato.Mensagem = Request.Form["mensagem"];
+
+            string conteudo = string.Format("Nome: {0}, Email: {1}, Assunto: {2}, Mensagem: {3}", contato.Nome, contato.Email, contato.Assunto, contato.Mensagem);
+            return new ContentResult() { Content = conteudo };
+        }
+        */
     }
 }
---
+++ Seção 04/Projeto1/Site1/Models/Contato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site1.Models {
+    public class Contato {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+
+    }
+}
