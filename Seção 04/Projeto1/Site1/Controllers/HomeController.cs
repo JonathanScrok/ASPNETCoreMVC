49a84ed0f3ac425d6e3bc5be214dfadbf4c8703e
JonathanScrok <jonathanwscrok@gmail.com>
2021-08-06 23:33:07 -0300

    Criação do Login

--- Seção 04/Projeto1/Site1/Controllers/HomeController.cs
+++ Seção 04/Projeto1/Site1/Controllers/HomeController.cs
@@ -1,15 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Site1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Site1.Controllers {
-    public class HomeController : Controller{
+    public class HomeController : Controller {
 
         public ActionResult Index() {
             //return new ContentResult() { Content = "Olá Mundo", ContentType = "text/plain" };
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login([FromForm] Usuario usuario) {
+
+            if (ModelState.IsValid) {
+
+                if (usuario.Email == "jonathanwscrok@gmail.com" && usuario.Senha == "1234") {
+
+                    return Redirect("/contato");
+
+                } else {
+
+                    ViewBag.Mensagem = "Os dados informados são inválidos!";
+                    return View();
+                }
+            } else {
+
+                return View();
+            }
+
+
         }
     }
 }
